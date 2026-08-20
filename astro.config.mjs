f0688ac58bb60c10782ea99ea0245a5820927ef2
Sam Tate <samtate@users.noreply.github.com>
2026-08-20 18:03:00 +0100

    Use production domain for local SEO builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: process.env.SITE_URL ?? 'https://blog.example.com',
+  site: process.env.SITE_URL ?? 'https://sa.mtate.me.uk',
   output: 'static',
   integrations: [sitemap()],
   markdown: {
